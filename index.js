db2e8e70d847c1538ea46f079f51a1594589e628
Md Ibrahim <mdrafi15429@gmail.com>
2022-05-23 21:25:35 +0600

    single service api loaded

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 
@@ -25,9 +25,13 @@ async function run(){
             const query ={};
             const curser =serviceCollection.find(query);
             const result = await curser.toArray()
+            res.send(result);     
+        })
+        app.get('/service/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query ={_id: ObjectId(id)};
+            const result = await serviceCollection.findOne(query);
             res.send(result);
-
-            
         })
 
     }
