f71f5d529af585c8c20c1491495464220b78c929
Md Ibrahim <mdrafi15429@gmail.com>
2022-05-24 18:14:32 +0600

    order api added

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
+
 const app = express()
+
 const cors = require('cors');
+
 require('dotenv').config();
+
 const jwt = require('jsonwebtoken');
+
 const port = process.env.PORT || 5000
+
 app.use(cors())
 app.use(express.json())
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
@@ -20,6 +27,8 @@ async function run(){
     try{
         await client.connect();
         const serviceCollection = client.db('garageTech').collection('service');
+        const orderCollection = client.db('garageTech').collection('order');
+
 
         app.get('/service',async(req,res)=>{
             const query ={};
@@ -27,12 +36,20 @@ async function run(){
             const result = await curser.toArray()
             res.send(result);     
         })
-        app.get('/service/:id',async(req,res)=>{
+        app.get('/service/:id',async(req, res)=>{
             const id = req.params.id;
-            const query ={_id: ObjectId(id)};
+            const query = {_id: ObjectId(id)}
             const result = await serviceCollection.findOne(query);
+            res.send(result)
+        })
+
+
+        app.post('/order',async(req,res)=>{
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
             res.send(result);
         })
+
 
     }
     finally{
